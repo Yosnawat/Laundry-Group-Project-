2925b334cd91ade89554f2faeb477a1afd4e9b31
Wacharaphong Sutthuboriban <6731503032@lamduan.mfu.ac.th>
2025-10-27 19:23:06 +0700

    6731503032

--- frontend/src/main/resources/CorsConfig.java
+++ frontend/src/main/resources/CorsConfig.java
@@ -1,4 +1,6 @@
-package th.mfu.config;
+
+
+import java.util.Arrays;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,8 +9,6 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Arrays;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
