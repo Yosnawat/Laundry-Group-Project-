a4ecd08bd795793c270c00d525c616c72e3dc190
Wacharaphong Sutthuboriban <6731503032@lamduan.mfu.ac.th>
2025-10-28 16:14:52 +0700

    6731503032

--- backend/src/main/java/App.java
+++ backend/src/main/java/App.java
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {"th.mfu", "service", "repo", "model", "config"})
+@SpringBootApplication(scanBasePackages = {"service", "repo", "model", "config"})
 @EnableJpaRepositories(basePackages = {"repo"})
 @EntityScan(basePackages = {"model"})
 public class App {
