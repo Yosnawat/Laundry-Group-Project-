175f9d982d4edb1c374c534a013aed6c7b333f1e
6731503032 <6731503032@lamduan.mfu.ac.th>
2025-10-31 01:32:31 +0700

    Update MachineRepository.java

--- backend/src/main/java/repo/MachineRepository.java
+++ backend/src/main/java/repo/MachineRepository.java
@@ -12,8 +12,6 @@ public interface MachineRepository extends JpaRepository<Machine, Long> {
 
     List<Machine> findByStatus(String status);
 
-    List<Machine> findByType(String type);
-
     Machine findByMachineNumber(String machineNumber);
 
     List<Machine> findByCurrentUserId(Long userId);
